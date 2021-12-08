6f2ca1618106c2b39a9c1fc5cfd8e3b1051139b0
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-08 13:41:33 +0700

    complete

---
+++ src/Lab/Button.js
@@ -0,0 +1,11 @@
+const Button = () => {
+  return (
+    <>
+      <button class="btn-add">+</button>
+      <span>0</span>
+      <button class="btn-subtract">-</button>
+      <button class="btn-reset">Reset</button>
+    </>
+  );
+};
+export default Button;
