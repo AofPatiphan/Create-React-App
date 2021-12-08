e66d73856a50c347a253c4e4f78cbc5784d1e58f
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-08 16:24:39 +0700

    lesson completed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  // // const handleClick = (event) => {
+  // //   return console.log(event);
+  // // };
+
+  // ฟังก์ชัน รับพารามีเตอร์เป็นฟังก์ชันซ้อนกันอีกที ถ้างงก็ใช้ handleClick1 ไป
+  // const handleClick2 = (id) => (event) => {
+  //   return console.log(id);
+  // };
+
+  // // อยากส่ง event กับ id ต้องใส่ annonymous function ใน button แล้วมาเขียนฟังชันแยก
+  // const handleClick1 = (event, id) => {
+  //   return console.log(id);
+  // };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    console.log("submited");
+  };
+
+  const handleChangeEmail = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* //     <button onClick={handleClick1}>Click Me</button> */}
+      {/* //     <button onClick={(event) => handleClick(event, 3)}>Click Me</button> */}
+
+      {/* <button onClick={handleClick2(5)}>Click Me</button> */}
+
+      <form onSubmit={handleSubmitForm}>
+        <input name="email" onChange={handleChangeEmail} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
