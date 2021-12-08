82381955d6183d77b31e58834d0d597ab9bba12f
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-08 19:13:04 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from "./logo.svg";
-// import "./App.css";
-import Welcome from "./Welcome";
-import styles from "./App.module.css";
+import "./App.css";
 
-// เก็บ style ไว้ในตัวแปร แล้วเอาไปใส่ inline
 function App() {
-  const divStyle = {
-    color: "red",
-    textAlign: "center",
-    margin: "20px 10px",
-    padding: "10px",
-  };
   return (
-    <div className={styles.container}>
-      <div style={divStyle}>Hello React Basic Styling</div>
-      <div className="text-success ms-5">Hello React Basic Styling</div>
-      <div className={styles["main-div"]}>Hello React Basic Styling</div>
-      <Welcome />
+    <div className="main-div">
+      <button className="btn1">+</button>
+      <span className="text">0</span>
+      <button className="btn1">-</button>
+      <button className="btn2">Reset</button>
     </div>
   );
 }
