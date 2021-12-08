bf328fc1cd3dadd46b7388b499edbf2cfdde2d35
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-08 18:30:29 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -2,39 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // // const handleClick = (event) => {
-  // //   return console.log(event);
-  // // };
-
-  // ฟังก์ชัน รับพารามีเตอร์เป็นฟังก์ชันซ้อนกันอีกที ถ้างงก็ใช้ handleClick1 ไป
-  // const handleClick2 = (id) => (event) => {
-  //   return console.log(id);
-  // };
-
-  // // อยากส่ง event กับ id ต้องใส่ annonymous function ใน button แล้วมาเขียนฟังชันแยก
-  // const handleClick1 = (event, id) => {
-  //   return console.log(id);
-  // };
-
-  const handleSubmitForm = (event) => {
-    event.preventDefault();
-    console.log("submited");
+  const handleClick = (event) => {
+    if (window.confirm("Leave for https://google.com?")) {
+    } else {
+      event.preventDefault();
+    }
   };
 
-  const handleChangeEmail = (event) => {
-    console.log(event.target.value);
-  };
   return (
     <div className="App">
-      {/* //     <button onClick={handleClick1}>Click Me</button> */}
-      {/* //     <button onClick={(event) => handleClick(event, 3)}>Click Me</button> */}
-
-      {/* <button onClick={handleClick2(5)}>Click Me</button> */}
-
-      <form onSubmit={handleSubmitForm}>
-        <input name="email" onChange={handleChangeEmail} />
-        <button>Submit</button>
-      </form>
+      <a href="https://google.com" onClick={handleClick}>
+        Google
+      </a>
     </div>
   );
 }
