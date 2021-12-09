a908ba125b9b919e7623b03d720734dd7440f67f
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-09 14:24:45 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -2,39 +2,27 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let state = useState(""); //ค่าเริ่มต้นที่จะให้โชว์ อันนี้ตั้งเป็น empty string return ค่าเป็น array โดย index 0 เก็บค่า state ปัจจุบัน,index 1 เก็บฟังก์ชันที่ใช้อัพเดทค่า state
-  let state1 = useState("");
-  let [country, setCountry] = useState("Thailand");
-  let [population, setPopulation] = useState("1000");
-  let value = "";
+  const [state, setState] = useState("");
+  const [visible, setVisible] = useState(true);
 
-  const handleChange = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    state[1](value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
-  };
-  const handleChange1 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    state1[1](value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
-  };
-  const handleChange2 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    setCountry(value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น "" , setCountry คือ index 1 ที่เก็บค่าที่พิมพ์เข้าไป
-  };
-  const handleChange3 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    setPopulation(value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
+  const handleClick = (event) => {
+    // value = "hide";
+    // state[0](value);
+    setVisible(!visible); // วิธีลัด ถ้างงให้ดูด้านล่าง
+
+    // if (visible === false) {
+    //   setVisible(true);
+    // } else {
+    //   setVisible(false);
+    // }
   };
 
   return (
     <div className="App">
-      <h1>{state[0]}</h1>
-      <input type="text" onChange={handleChange} />
-      <h1>{state1[0]}</h1>
-      <input type="text" onChange={handleChange1} />
-      <h1>{country}</h1>
-      <input type="text" onChange={handleChange2} />
-      <h1>{population}</h1>
-      <input type="text" onChange={handleChange3} />
+      <button onClick={handleClick}>
+        {visible ? "Click to hide " : "Click to show"}
+      </button>
+      <h1 className={visible ? "" : "hide"}>Click button to hide me</h1>
     </div>
   );
 }
