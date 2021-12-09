43ebd53debf8fabdaec9b7780cf3d48442d02423
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-09 12:11:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,40 +2,17 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let state = useState(""); //ค่าเริ่มต้นที่จะให้โชว์ อันนี้ตั้งเป็น empty string return ค่าเป็น array โดย index 0 เก็บค่า state ปัจจุบัน,index 1 เก็บฟังก์ชันที่ใช้อัพเดทค่า state
-  let state1 = useState("");
-  let [country, setCountry] = useState("Thailand");
-  let [population, setPopulation] = useState("1000");
-  let value = "";
-
-  const handleChange = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    state[1](value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
+  const [count, setCount] = useState(0);
+  const handleClickAdd = () => {
+    // setCount(5);
+    // setCount(count + 1);
+    setCount((prevState) => prevState + 1);
   };
-  const handleChange1 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    state1[1](value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
-  };
-  const handleChange2 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    setCountry(value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น "" , setCountry คือ index 1 ที่เก็บค่าที่พิมพ์เข้าไป
-  };
-  const handleChange3 = (event) => {
-    value = event.target.value; //ประกาศตัวแปรมารับค่าที่พิมพ์เข้าไป
-    setPopulation(value); // ให้ state ถัดมาเป็นค่าที่พิมพ์เข้าไป //ของเดิม state[0] เป็น ""
-  };
-
   return (
-    <div className="App">
-      <h1>{state[0]}</h1>
-      <input type="text" onChange={handleChange} />
-      <h1>{state1[0]}</h1>
-      <input type="text" onChange={handleChange1} />
-      <h1>{country}</h1>
-      <input type="text" onChange={handleChange2} />
-      <h1>{population}</h1>
-      <input type="text" onChange={handleChange3} />
-    </div>
+    <>
+      <h1>{count}</h1>
+      <button onClick={handleClickAdd}>+</button>
+    </>
   );
 }
 
