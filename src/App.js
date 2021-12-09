c83769d4a8d738a661eedff550e51c40e25a830f
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-09 14:54:36 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState("");
   return (
     <div className="App">
       <label>Enter Dollar:</label>
-      <input type="text" />
-      <p>Convert to Bath: {}</p>
+      <input
+        type="text"
+        onChange={(event) => setAmount(event.target.value)}
+        value={amount}
+      />
+      <p>
+        Convert to Baht:
+        <span style={{ color: "red" }}> {amount * 30}</span> Baht
+      </p>
     </div>
   );
 }
